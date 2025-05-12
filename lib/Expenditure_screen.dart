2bebf9c0e48d319d76c2ed3413ef1e9db97ba25d
MSI\masa_ <masarito0205@gmail.com>
2025-05-12 10:39:25 +0900

    収入支出入力画面

---
+++ lib/Expenditure_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/final_exam.dart';
+
+class Income_screen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title : Text("支出追加画面")
+      ),
+      body : Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: '支出の内容'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: '金額'),
+            ), // 画像とボタンの間にスペースを追加
+            TextButton(
+              child: Text("ホーム画面に戻る"),
+              // （1） 前の画面に戻る
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(
+                  // （2） 実際に表示するページ(ウィジェット)を指定する
+                  builder: (context) => final_exam()
+                ));
+              },
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
--- lib/final_exam.dart
+++ lib/final_exam.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/HomePage.dart';
+import 'package:flutter_application_1/Income_Screen.dart';
 
 class final_exam extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title : Text("期末試験専用画面")
+        title : Text("家計簿")
       ),
       body : Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/images/c_img.jpg'), // 画像を表示
+            Text('ようこそ！家計簿アプリへ', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text('収入を追加'),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(
+                  // （2） 実際に表示するページ(ウィジェット)を指定する
+                  builder: (context) => Income_screen()
+                ));
+              },
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/income'),
+              child: Text('支出を追加'),
+            ),
+            //Image.asset('assets/images/c_img.jpg'), // 画像を表示
             SizedBox(height: 20), // 画像とボタンの間にスペースを追加
             TextButton(
               child: Text("最初の画面に戻る"),
